4d469b9cf45d0eee6ff952a74051d1e3bbc06c3f
ryanrdunn <dunnryanroderick@gmail.com>
2023-10-12 15:25:29 +0100

    Fixed vpc

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -44,8 +44,8 @@ module "blog_sg"  {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  name    = "blog_sg"
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
