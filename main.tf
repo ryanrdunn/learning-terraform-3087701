76981ef31941111fc9dab989a0466a548139ffd9
ryanrdunn <dunnryanroderick@gmail.com>
2023-10-12 15:18:11 +0100

    renamed sg module

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
+
   tags = {
     Name = "HelloWorld!"
   }
@@ -44,7 +44,7 @@ module "blog_sg"  {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  name    = "blog"
+  name    = "blog_sg"
   vpc_id  = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
